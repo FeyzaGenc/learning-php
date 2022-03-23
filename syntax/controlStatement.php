0e103943ff7cbad558a4034f94971bf4c29eb3b5
FeyzaGenc <85829974+FeyzaGenc@users.noreply.github.com>
2022-03-23 17:53:02 +0300

    upd

--- syntax/controlStatement.php
+++ syntax/controlStatement.php
@@ -2,6 +2,13 @@
     $season=array("summer","winter","spring","autumn");
     foreach($season as $arr)
     {
-        echo "season is:$arr<br/>";
+        echo "season is:$arr<br/><br>";
+    }
+
+    $employee=array("Name"=>"Alex","Email"=>"alex_jtp@gmail.com","Age"=>21,"Gender=>male");
+    foreach($employee as $key=>$element)
+    {
+        echo $key.":".$element;
+        echo "</br>";
     }
 ?>
